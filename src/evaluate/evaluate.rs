4f20d884c3601a38ce8e593ee61c40dc3d82b988
Mikhail Morozov <inferst@gmail.com>
2024-08-07 19:07:05 +0300

    codecrafters submit [skip ci]

--- src/evaluate/evaluate.rs
+++ src/evaluate/evaluate.rs
@@ -1,13 +1,48 @@
-use crate::parser::Expr;
+use core::fmt;
 
-pub fn evaluate(expr: &Expr) -> String {
+use crate::parser::{Expr, UnaryOperator};
+
+pub enum Literal {
+    Boolean(bool),
+    Number(f64),
+    String(String),
+    Nil,
+}
+
+impl fmt::Display for Literal {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Literal::Boolean(bool) => write!(fmt, "{bool}"),
+            Literal::Number(number) => write!(fmt, "{number}"),
+            Literal::String(string) => write!(fmt, "{string}"),
+            Literal::Nil => write!(fmt, "nil"),
+        }
+    }
+}
+
+pub fn evaluate(expr: &Expr) -> Literal {
     match expr {
-        Expr::True => "true".to_string(),
-        Expr::False => "false".to_string(),
-        Expr::Nil => "nil".to_string(),
-        Expr::String(string) => string.to_owned(),
-        Expr::Number(number) => number.to_string(),
-        Expr::Unary(_, _) => todo!(),
+        Expr::True => Literal::Boolean(true),
+        Expr::False => Literal::Boolean(false),
+        Expr::Nil => Literal::Nil,
+        Expr::String(string) => Literal::String(string.clone()),
+        Expr::Number(number) => Literal::Number(*number),
+        Expr::Unary(operator, expr) => {
+            let expr = expr.as_ref();
+            let literal = evaluate(expr);
+            match operator {
+                UnaryOperator::Bang => match literal {
+                    Literal::Boolean(bool) => Literal::Boolean(!bool),
+                    Literal::Number(number) => Literal::Boolean(number == 0.0),
+                    Literal::String(string) => Literal::Boolean(string.is_empty()),
+                    Literal::Nil => Literal::Boolean(true),
+                },
+                UnaryOperator::Minus => match literal {
+                    Literal::Number(number) => Literal::Number(-number),
+                    _ => panic!("aboba"),
+                },
+            }
+        }
         Expr::Binary(_, _, _) => todo!(),
         Expr::Grouping(expr) => evaluate(expr),
     }
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -2,4 +2,5 @@ mod expr;
 mod parser;
 
 pub(crate) use expr::Expr;
+pub(crate) use expr::UnaryOperator;
 pub(crate) use parser::parse_tokens;
